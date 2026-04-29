5447dc9a2fcf4a95d62fe974733a17c1a8375af0
Benjamín Panichelli <benjaminpanichelli@gmail.com>
2026-04-28 21:00:51 -0300

    Se agrego archivo formulario

---
+++ src/componentes/Formulario.jsx
@@ -0,0 +1,48 @@
+import {useState} from "react";
+
+export default function Formulario() {
+    const [documento, setDocumento] = useState("");
+    const [apellidos, setApellidos] = useState("");
+    const [nombres, setNombres] = useState("");
+    const [rol, setRol] = useState("");
+
+    const hanlderSubmit = (e) => {
+        e.preventDefault();
+        console.log({documento, apellidos, nombres, rol});
+    }
+    
+    return(
+        <div className="Formulario">
+            <h1>Componente Formulario</h1>
+            <form onSubmit={hanlderSubmit}>
+                <input
+                    type="text"
+                    placeholder="Documento"
+                    onChange={(e) => setDocumento(e.target.value)}
+                    value={documento}
+                />
+
+                <input
+                    type="text"
+                    placeholder="Apellido"
+                    onChange={(e) => setApellidos(e.target.value)}
+                    value={apellidos}
+                />
+                <input
+                    type="text"
+                    placeholder="Nombre"
+                    onChange={(e) => setNombres(e.target.value)}
+                    value={nombres}
+                />
+                <select
+                    onChange={(e) => setRol(e.target.value)}
+                    value={rol}
+                >
+                    <option value="alumno">Alumno</option>
+                    <option value="docente">Docente</option>
+                </select>
+                <button type="submit">Guardar</button>
+            </form>
+        </div>
+    )
+}
